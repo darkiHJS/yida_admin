c88bd759e7547c1de5905a88d69699a5d4ed8595
huan <125730892@qq.com>
2025-04-10 17:31:11 +0800

    今天的科研就到这吧

--- src/hooks/useTable.ts
+++ src/hooks/useTable.ts
@@ -1,6 +1,7 @@
-import type { TableProps } from "antd"
+import { Form, type TableProps } from "antd"
 import type { FormInstance } from "antd/es/form"
-import type { Key } from "react"
+import { useEffect, useMemo, useState, type Key } from "react"
+import { useLocation } from "react-router-dom"
 
 export interface BasePageList<T> {
 	total: number
@@ -47,4 +48,145 @@ interface UseTableOptions<
 	key: string
 	idKey?: string
 	cacheEnabled?: boolean
+	dataStaleTime?: number
+	pagination?: boolean
+	selectable?: boolean
+	formInitialvalues?: Record<string, any>
+	columns: TableProps<any>["columns"]
+	scrollX?: string | number
+	scrollY?: string | number
+	pageCreatePath?: string
+	pageEditPath?: string
+}
+
+function isPageResponse<T>(data: ApiResponse<T>): data is BasePageList<T> {
+	return !Array.isArray(data) && "total" in data && "list" in data
+}
+
+export function useTable<
+	TApiFn extends (params: any) => Promise<ApiResponse<any>>
+>({
+	listApiFn,
+	deleteApiFn,
+	batchDeleteApiFn,
+	key,
+	idKey = "id",
+	cacheEnabled = true,
+	dataStaleTime = 60000,
+	pagination = true,
+	selectable = false,
+	formInitialvalues = {},
+	columns,
+	scrollX = "100%",
+	scrollY,
+	pageCreatePath,
+	pageEditPath,
+}: UseTableOptions<TApiFn>) {
+	// const {} = usePageTransfer()
+
+	const needsForm = Object.keys(formInitialvalues).length > 0
+
+	// 创建form实例
+	const [form] = needsForm ? Form.useForm() : [null]
+
+	const location = useLocation()
+
+	// --------- 状态 ------------
+	// 分页
+	const [page, setPage] = useState(1)
+	const [pageSize, setPageSize] = useState(10)
+
+	// 查询
+	const [queryState, setQueryState] =
+		useState<Record<string, any>>(formInitialvalues)
+
+	// 选择状态
+	const [selectedState, setSelectedState] = useState<number[]>([])
+	const selectedCount = useMemo(() => selectedState.length, [selectedState])
+	const selectedIsEmpty = useMemo(() => selectedCount === 0, [selectedCount])
+
+	const listQueryKey = `${key}-list`
+	const stateQueryKey = `${key}-list-state`
+
+	// // 初始化 cache 状态
+	// useEffect(() => {
+
+	// }, [])
+
+	// --------- query & data fetching -----------
+	// const {} = useQuery({
+
+	// })
+	const isLoading = false
+	const refetch = () => {}
+	// ------- action -------
+	const handleSearch = async () => {
+		try {
+			const values = await form?.validateFields()
+		} catch (error) {
+			console.error("查询失败：", error)
+		}
+	}
+	// ------ Computed Properties ------
+	const list: never[] = []
+	const total = 0
+	const handlePageChange = (page: number, pageSize: number) => {
+		setPage(page)
+		setPageSize(pageSize)
+	}
+	// ------ table props ------
+	const tableProps = useMemo(
+		() => ({
+			rowKey: idKey,
+			columns,
+			dataSource: list,
+			loading: isLoading,
+			sticky: true,
+			...(selectable
+				? {
+						rowSelection: {
+							type: "checkbox" as const,
+							selectedRowKeys: selectedState,
+							onChange: (selectedRowKeys: Key[]) => {
+								setSelectedState(selectedRowKeys.map((key) => Number(key)))
+							},
+						} as TableProps<any>["rowSelection"],
+				  }
+				: {}),
+			scroll: {
+				x: scrollX,
+				y: scrollY,
+			},
+			pagination: pagination
+				? {
+						current: page,
+						pageSize,
+						total,
+						onChange: handlePageChange,
+				  }
+				: (false as const),
+		}),
+		[
+			idKey,
+			columns,
+			list,
+			isLoading,
+			selectable,
+			selectedState,
+			scrollX,
+			scrollY,
+			pagination,
+			page,
+			pageSize,
+			total,
+			refetch,
+		]
+	)
+
+	return {
+		form: form as FormInstance,
+
+		// 表格
+		tableProps,
+	}
 }
--- src/pages/system/menu/index.tsx
+++ src/pages/system/menu/index.tsx
@@ -1,7 +1,29 @@
+import { useTable } from "@/hooks/useTable"
 import { Divider, Form, Table, Tag } from "antd"
 
 const Menu = () => {
-	return <div>Menu</div>
+	const { tableProps } = useTable({
+		key: "menu",
+		pagination: false,
+		listApiFn: async () => {
+			await new Promise((resolve) => setTimeout(resolve, 1000))
+		},
+		deleteApiFn: async () => {
+			await new Promise((resolve) => setTimeout(resolve, 1000))
+		},
+		scrollY: 300,
+		columns: [
+			{ title: "菜单名称", dataIndex: "name", key: "name" },
+			{ title: "菜单路径", dataIndex: "path", key: "path" },
+			{ title: "菜单类型", dataIndex: "type", key: "type" },
+		],
+	})
+	return (
+		<div>
+			Menu
+			<Table {...tableProps} />
+		</div>
+	)
 }
 
 export default Menu
